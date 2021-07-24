f6a27e122ad0c6ddc89e5fce3c465cce485de794
Your Namegit <you@example.com>
2021-07-24 20:58:52 -0300

    form event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-import "./assets/App.css";
+import "../src/assets/app.css";
 import "./assets/index.css";
 class App extends Component {
+  criarNota(titulo, texto) {
+    console.log(`uma nova nota foi criada ` + titulo + " " + texto);
+  }
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
+        <FormularioCadastro criarNota={this.criarNota} />
         <ListaDeNotas />
       </section>
     );
