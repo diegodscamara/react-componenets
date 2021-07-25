fc68eb7a0b242134ea91e34d3bbec66f5262f718
Your Namegit <you@example.com>
2021-07-24 21:27:54 -0300

    set state dynamic values

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "../src/assets/app.css";
 import "./assets/index.css";
 class App extends Component {
-  criarNota(titulo, texto) {
-    console.log(`uma nova nota foi criada ` + titulo + " " + texto);
+  constructor() {
+    super();
+
+    this.state = {
+      notas: [],
+    };
   }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas,
+    };
+    this.setState(novoEstado);
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota} />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
